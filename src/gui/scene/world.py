5808e536a2682b6efdbd4f463a9b7086a795feb9
Christopher Clouse <chc2t@mtmail.mtsu.edu>
2019-01-26 13:40:52 -0600

    adding scene stuff

--- src/gui/scene/world.py
+++ src/gui/scene/world.py
@@ -1,3 +1,5 @@
+from entity.player import *
+from gui.hud import *
 from map.tile_map import *
 
 class World(Scene):
@@ -6,3 +8,19 @@ class World(Scene):
 		super(World, self).__init__()
 		self.__tilemap = TileMap()
 		self.__players = [Player(), Player()]
+		self.__hud = Hud()
+
+	def draw(self):
+		self.__tilemap.draw()
+		for player in self.__players:
+			player.draw()
+		self.__hud.draw()
+
+	def tick(self, dt):
+		self.__tilemap.tick(dt)
+		for player in self.__players:
+			player.tick(dt)
+		self.__hud.tick(dt)
+
+	def get_players(self):
+		return self.__players
--- src/gui/screen.py
+++ src/gui/screen.py
@@ -5,7 +5,7 @@ class Screen:
 	def __init__(self, resolution=(1280, 720)):
 		self.__resolution = resolution
 		self.__surface = pygame.display.set_mode(resolution)
-		#self.__surface = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
+		self.__surface = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
 		self.__scene = None
 
 	def blit(self, *args, **kwargs):
@@ -22,22 +22,22 @@ class Screen:
 
 	def draw(self):
 		self.__surface.fill((0, 0, 0))
-#		self.__scene.draw(self)
+		self.__scene.draw(self)
 		pygame.display.flip()
 
 	def tick(self, dt):
-#		self.__scene.tick(dt)
+		self.__scene.tick(dt)
 		pass
 
 	def get_resolution(self):
 		return self.__resolution
 
-#	def get_scene(self):
-#		return self.__scene
-#
-#	def set_scene(self, scene):
-#		if self.__scene:
-#			self.__scene.clean()
-#			del self.__scene
-#		self.__scene = scene
-#		self.__scene.ready()
+	def get_scene(self):
+		return self.__scene
+
+	def set_scene(self, scene):
+		if self.__scene:
+			self.__scene.clean()
+			del self.__scene
+		self.__scene = scene
+		self.__scene.ready()
