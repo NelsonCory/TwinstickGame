359d598669822eb1b325d3f97ff03a4410db1d91
Cory Nelson <cln4d@mtmail.mtsu.edu>
2019-01-26 13:44:08 -0600

    Added accessors and setters to entities

--- src/entity/player.py
+++ src/entity/player.py
@@ -8,11 +8,13 @@ class Player(Entity):
 		self.__skin = None
 		self.__hp = 100
 		self.__shield = 100
+		self.__current_heat = 0
+		self.__MAX_HEAT = 100
 
 	def draw(self):
 		pass
 
-	def update(self, dt):
+	def update(self, dt): 
 		pass
 
 	def get_hp(self):
@@ -20,3 +22,9 @@ class Player(Entity):
 
 	def get_shield(self):
 		return self.__shield
+
+	def get_heat(self):
+		return self.__current_heat
+	
+	def is_overheated(self):
+		return self.__current_heat >= self.__MAX_HEAT
