467386d55c5a7d8a54e652b6c587b77387fd89b8
Raine Taylor <trainewillow@gmail.com>
2019-01-26 14:29:53 -0600

    working on tilemap

--- src/map/tile.py
+++ src/map/tile.py
@@ -4,6 +4,7 @@ class Tile():
 	def __init__(self):
 		self.__t_id = None
 		self.__solid = None
+		self.__spawn = None
 
 	def get_t_id(self):
 		return self.__t_id
@@ -11,12 +12,19 @@ class Tile():
 	def get_solid(self):
 		return self.__solid
 
+	def get_spawn(self):
+		return self.__spawn
+
 	def set_t_id(self, t_id):
 		self.__t_id = t_id
 
 	def set_solid(self, solid):
 		self.__solid = __solid
 
+	def set_spawn(self, spawn):
+		self.__spawn = __spawn
+
 	def load_tile(self, data):
 		self.__t_id = int(data.split()[0])
 		self.__solid = int(data.split()[1])
+		self.__spawn = int(data.split()[2])
--- src/map/tile_map.py
+++ src/map/tile_map.py
@@ -1,5 +1,20 @@
 class TileMap:
+
+	__instance = None
+
 	def __init__(self):
+		self.__chunk[[]]
+
+		self.__frame = 0
+
+		TileMap.__instance = self
+
+
+	@staticmethod
+	def get_instance():
+		return TileMap.__instance
+
+	def load_map(self, path):
 		pass
 
 	def draw(self):
