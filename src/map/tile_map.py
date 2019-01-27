ca10935665f843b4b061a3472a72d4dba043afc9
Raine Taylor <trainewillow@gmail.com>
2019-01-26 20:06:49 -0600

    stuff with the tile editor

--- src/map/tile_map.py
+++ src/map/tile_map.py
@@ -1,5 +1,6 @@
 import pygame
-from tile import *
+from . tile import *
+from core.resource_manager import *
 
 
 class TileMap:
@@ -10,13 +11,18 @@ class TileMap:
 		self.__chunk = [[Tile() for _ in range(22)] for _ in range(40)]
 
 		self.__frame = 0
-
+		self.init_map()
 		TileMap.__instance = self
 
 
 	@staticmethod
 	def get_instance():
 		return TileMap.__instance
+
+	def init_map(self):
+		for x in range(40):
+			for y in range(22):
+				self.__chunk[x][y].load_tile("0 0 0")
 
 	def load_map(self, path):
 		lines = [line.rstrip('\n') for line in open(path)]
@@ -37,10 +43,13 @@ class TileMap:
 		res = ResourceManager.get_instance()
 		for x in range(40):
 			for y in range(22):
-				scrn.blit(res.get_tile(self.__chunk[x][y].get_t_id(0), 0, self.__frame), (x*32, y*32))
+				scrn.blit(res.get_tile(self.__chunk[x][y].get_t_id(), 0, self.__frame), (x*32, y*32))
 
 	def update(self, dt=0):
 		pass
 
 	def get_tile(self, x, y):
 		return self.__chunk[x][y]
+
+	def set_tile(self, x, y, tile):
+		self.__chunk[x][y] = tile
--- src/tile_editor.py
+++ src/tile_editor.py
@@ -20,10 +20,24 @@ rm = ResourceManager(sys.argv[0])
 
 tm = TileMap()
 
+currTile = Tile()
+
+currTile.load_tile("0 0 0")
+
 while True: # the main game loop
 	DISPLAYSURF.fill((0, 0, 0))
 
-	tm.draw()
+	tm.draw(DISPLAYSURF)
+
+	mouse = pygame.mouse.get_pos()
+
+	mx = mouse[0]
+	my = mouse[1]
+	tmx = mx // 32
+	tmy = my // 32
+	print(str(tmx) + " " + str(tmy))
+
+	pygame.draw.rect(DISPLAYSURF, (90, 0, 50), (tmx*32, tmy*32, mx*32+32, my*32+32), 2)
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
